1b6a2079d45021f53a1cf3e18e7101af1583dea3
Jesse Smith <jesse.etan.smith@gmail.com>
2017-05-14 18:03:18 -0400

    set up proper session with env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require("express"),
     bodyParser = require("body-parser"),
     mongodb = require("mongodb"),
@@ -5,23 +7,22 @@ var express = require("express"),
     session = require("express-session"),
     fs = require("fs");
 
+var dbUsername = process.env.MONGONAME;
+var dbPassword = process.env.MONGOPASS;
+var sessionPass = process.env.SESSIONPASS;
+
 var app = express();
-
-
-var dbusername = process.env.MONGONAME;
-var dbpassword = process.env.MONGOPASS;
-var sessionpass = process.env.SESSIONPASS;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({secret: sessionpass,
-                 cookie: {maxAge: 60000},
-                 resave: true,
-                 saveUninitialized: true}));
+app.use(session({
+  secret: sessionPass,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.static(__dirname + "/public"));
 
-var uri = "mongodb://" + dbusername + ":" + dbpassword + "@ds013366.mlab.com:13366/youcapital";
+var uri = "mongodb://" + dbUsername + ":" + dbPassword + "@ds013366.mlab.com:13366/youcapital";
 var db = mongojs(uri, ["users"]);
 
 app.listen(process.env.PORT || 3000, function() {
@@ -29,5 +30,5 @@ app.listen(process.env.PORT || 3000, function() {
 });
 
 app.get("/", function(req, res) {
-
+  console.log("Youcapital online!");
 });
